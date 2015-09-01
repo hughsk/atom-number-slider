d8b11e8e4a242fa843cd104abd5bd7c6a9e3e857
Hugh Kennedy <hughskennedy@gmail.com>
2015-09-01 15:19:57 -0400

    .

---
+++ README.md
@@ -0,0 +1,23 @@
+# number-slider
+
+[![stable](http://badges.github.io/stability-badges/dist/stable.svg)](http://github.com/badges/stability-badges)
+
+Interactive draggable sliders for your numbers in Atom!
+
+![](http://i.imgur.com/iXfBppt.gif)
+
+Useful, for example, when debugging live-reloading shaders, interactives or visualizations.
+
+## Usage
+
+By default, you'll need to enable *number-slider* once you've installed it. You can do so using the following commands in the Command Palette:
+
+* `number-slider:enable`
+* `number-slider:toggle`
+* `number-slider:disable`
+
+Alternatively, you may set *number-slider* to always be on by changing the package settings.
+
+## License
+
+MIT, see [LICENSE.md](http://github.com/hughsk/atom-number-slider/blob/master/LICENSE.md) for details.
---
+++ index.js
@@ -0,0 +1,127 @@
+const meta = require('meta-keys')(window, true)
+const Disposable = require('atom').Disposable
+const findup = require('findup-element')
+
+exports.activate = activate
+exports.config = {
+  enabledByDefault: {
+    'type': 'boolean',
+    'default': false,
+    'title': 'Enable By Default',
+    'description': 'Next time your editor starts up, number-slider will be enabled by default'
+  }
+}
+
+function activate () {
+  atom.workspace.getTextEditors().forEach(attach)
+  atom.workspace.onDidAddTextEditor(function (d) {
+    return attach(d.textEditor)
+  })
+
+  atom.commands.add('atom-workspace', 'number-slider:disable', setState(false))
+  atom.commands.add('atom-workspace', 'number-slider:enable', setState(true))
+  atom.commands.add('atom-workspace', 'number-slider:toggle', setState())
+
+  setState(atom.config.get('number-slider.enabledByDefault'))()
+}
+
+var enabled = false
+function setState (_enabled) {
+  return function () {
+    if (typeof _enabled === 'undefined') _enabled = !enabled
+    if ((enabled = _enabled)) {
+      document.body.classList.add('number-slider-enabled')
+    } else {
+      document.body.classList.remove('number-slider-enabled')
+    }
+  }
+}
+
+function attach (editor) {
+  const view = atom.views.getView(editor)
+  const subs = []
+
+  var clientX = null
+  var range = null
+  var check = null
+  var value = null
+
+  view.shadowRoot.addEventListener('mousedown', mousedown, false)
+  window.addEventListener('mousemove', mousemove, true)
+  window.addEventListener('mouseup', clear, true)
+
+  function mousedown (e) {
+    if (!enabled) return
+
+    const el = findup(e.target, function (el) {
+      return el.classList && el.classList.contains('numeric')
+    })
+
+    if (!el) return
+
+    const operator = (
+      el.previousElementSibling &&
+      el.previousElementSibling.innerHTML
+    )
+
+    range = editor.bufferRangeForScopeAtCursor('.numeric')
+    if (!range) return
+    if (operator === '-' || operator === '+') {
+      range.start.column = Math.max(0, range.start.column - 1)
+    }
+
+    value = Number(editor.getTextInBufferRange(range))
+    if (isNaN(value)) return
+
+    check = editor.getBuffer().createCheckpoint()
+    editor.setSelectedBufferRange(range)
+    clientX = e.clientX
+
+    document.body.classList.add('number-slider-down')
+  }
+
+  function mousemove (e) {
+    if (!range) return
+    const diff = e.clientX - clientX
+    const nval = fixRounding(value + Math.round(diff / 2) * getAmplitude(value))
+
+    range = editor.setTextInBufferRange(range, String(nval))
+    editor.setSelectedBufferRange(range)
+
+    e.stopPropagation()
+    e.preventDefault()
+    return false
+  }
+
+  function clear () {
+    editor.getBuffer().groupChangesSinceCheckpoint(check)
+
+    check = null
+    range = null
+    document.body.classList.remove('number-slider-down')
+    document.body.style.cursor = null
+  }
+
+  subs.push(new Disposable(function () {
+    view.shadowRoot.removeEventListener('mousedown', mousedown, false)
+    window.removeEventListener('mousemove', mousemove, true)
+    window.removeEventListener('mouseup', clear, true)
+  }))
+
+  subs.push(editor.onDidDestroy(function () {
+    subs.forEach(function (sub) { return sub.dispose() })
+    subs.length = 0
+  }))
+}
+
+function getAmplitude (value) {
+  if (meta.shift[0]) return 10
+  if (meta.shift[1]) return 10
+  if (meta.alt[0]) return 1
+  if (meta.alt[1]) return 1
+  return Math.min(1, Math.pow(10, Math.floor(Math.log10(value)))) || 1
+}
+
+function fixRounding (number) {
+  return Math.round(number * 10000) / 10000
+}
